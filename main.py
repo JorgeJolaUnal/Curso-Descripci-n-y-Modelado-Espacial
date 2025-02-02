efb8a7d82dff0b8c5af6cbca9c8ce8e928c97875
JorgeJolaUnal <jjola@unal.edu.co>
2025-02-02 13:51:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 import subprocess
 main = Blueprint('main', __name__)
 
@@ -11,15 +11,7 @@ def tarea1():
 def tarea2():
     return render_template('tarea2.html')
 
-@main.route('/tarea3',methods=['GET', 'POST'])
+@main.route('/tarea3', methods=['GET', 'POST'])
 def tarea3():
-    if request.method == 'POST':
-        test_type = request.form['test_type']
-        cell_size = int(request.form['cell_size'])
-        if test_type == 'fisher':
-            result = subprocess.run(['Rscript', 'run_fisher_test.R', str(cell_size)], capture_output=True, text=True)
-        elif test_type == 'chi':
-            result = subprocess.run(['Rscript', 'run_chi_test.R', str(cell_size)], capture_output=True, text=True)
-        return render_template('result.html', result=result.stdout)
-    return render_template('tarea3.html')
+    return redirect("https://f0av15-jorge0andres-jola0hernandez.shinyapps.io/Tarea3/")
 
