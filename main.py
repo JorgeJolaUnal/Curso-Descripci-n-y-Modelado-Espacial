f4c51786f614a7e81f89783128156e1428aa3d6f
JorgeJolaUnal <jjola@unal.edu.co>
2025-02-02 12:50:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,14 +17,11 @@ def tarea3():
         test_type = request.form['test_type']  # 'chi' o 'fisher'
         cell_size = int(request.form['cell_size'])  # Tamaño de la celda
         
-        # Ruta completa al ejecutable Rscript
-        rscript_path = '/usr/local/bin/Rscript'  # Ajusta esta ruta según la ubicación de Rscript en tu contenedor
-        
         # Ejecutar el script en R, pasando el tamaño de la celda
         if test_type == 'fisher':
-            result = subprocess.run([rscript_path, 'run_fisher_test.R', str(cell_size)], capture_output=True, text=True)
+            result = subprocess.run(['run_fisher_test.R', str(cell_size)], capture_output=True, text=True)
         elif test_type == 'chi':
-            result = subprocess.run([rscript_path, 'run_chi_test.R', str(cell_size)], capture_output=True, text=True)
+            result = subprocess.run(['run_chi_test.R', str(cell_size)], capture_output=True, text=True)
         
         # Pasar el resultado del test a la plantilla de resultados
         return render_template('result.html', result=result.stdout)
